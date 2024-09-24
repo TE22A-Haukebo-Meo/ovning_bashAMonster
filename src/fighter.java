8406a21916b851427903950396859f2b41a38b3b
TE22A-Meo-Haukebo <meo.haukebo@elev.ga.ntig.se>
2024-09-24 10:22:09 +0200

    gjort så att användaren väljer skada

--- src/fighter.java
+++ src/fighter.java
@@ -1,10 +1,13 @@
+import java.util.Scanner;
+
 public class fighter {
+    Scanner tb = new Scanner(System.in);
     String name;
     int hp = 100;
 
     int attack(int hp){
         System.out.println(name+" attacked!");
-        hp -=10;
+        hp -= tb.nextInt();
         return hp;
     }
 }
